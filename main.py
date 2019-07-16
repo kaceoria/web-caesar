582ade35628a2abd8cd12238065a496890126432
Kaycee Williams <kaceoria@icloud.com>
2019-07-15 19:35:49 -0500

    Finish web caesar

--- main.py
+++ main.py
@@ -8,26 +8,26 @@ form = """
     <html>
         <head>
             <style>
-                form {
+                form.format(...) {{
                     backgroud-color: #eee;
                     padding: 20px;
                     margin: 0 auto;
                  width: 540px;
                  font: 16px sans-serif;
                  bordere-radius: 10px;
-             }
-                testarea {
+                 }}
+                textarea {{
                   margin: 10px 0;
                   width: 540px;
                   height: 120px;
-              }
+                  }}
             </style>
         </head>
         <body>
-            <form action="/action_page.php" method="POST">
+            <form action = "/encrypt" method="POST">
                 Rotate By:<input type="text" name="rot" value="0">
                 <br>
-                <textarea type="text" name="text"></textarea>
+                <textarea type="text" name="text">{0}</textarea>
                 <br>
                 <input type="submit" value="Submit">
             </form>
@@ -36,13 +36,13 @@ form = """
 
 @app.route("/")
 def index():
-    return form
+    return form.format("")
 
 @app.route("/encrypt", methods=["POST"])
 def encrypt():
     rots = int(request.form['rot'])
     msg = request.form['text']
-    #e_msg = (rotate_string(msg,rots))
-    return rotate_string(msg,rots)
+    e_msg = (rotate_string(msg,rots))
+    return form.format(e_msg)
      
 app.run()
